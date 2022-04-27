20fd170438b30dc1646c29621d4fb5e2555ea2ef
mrksj505 <sosjakub@gmail.com>
2022-04-27 22:21:19 +0200

    first commit

---
+++ src/main/java/com/kuchnia2/model/Meal.java
@@ -0,0 +1,12 @@
+package com.kuchnia2.model;
+
+import java.util.ArrayList;
+
+public class Meal {
+	private Integer id_meal;
+	private String name;
+	private String kind;
+	private String country;
+	private String time_to_cook;
+	private ArrayList<String> ingredients;
+}
